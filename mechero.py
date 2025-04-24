b49af223cf5d73a05464de4b687226697dd4f824
Miray Topcu <miraytopcup@gmail.com>
2025-04-24 14:16:24 +0300

    first draft of the project

---
+++ mechero.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
